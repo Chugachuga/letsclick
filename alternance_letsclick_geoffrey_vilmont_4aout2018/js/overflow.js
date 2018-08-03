bc4850eed2d1430ba40a6c548708e346994b7319
Geoffrey Vilmont <geoffreyvilmont@Geoffreys-MacBook-Pro.local>
2018-08-03 14:47:47 +0200

    add responsive css

--- alternance_letsclick_geoffrey_vilmont_4aout2018/js/overflow.js
+++ alternance_letsclick_geoffrey_vilmont_4aout2018/js/overflow.js
@@ -1,7 +1,15 @@
 'use strict';
 
-var width = $('body').width();
-var element = $('.element')[0];
-var outercontainer = $('.outer-container')[0];
-element.style.width = width * 600 / 1425 + "px";
-outercontainer.style.width = width * 600 / 1425 + "px";
+var width = $('body').width();//on obtient la valeur de la largeur du body
+var element = $('.element')[0];//on stock les données de type css dans une variable
+var outercontainer = $('.outer-container')[0];// ici aussi, afin de pouvoir faire des modifs dessus
+
+if (width >= 870) //Si la largeur de l'ecran est superieure a 870px; le container fera 50% de celle-ci, sinon 90%
+{
+  element.style.width = width * 600 / 1425 + "px";
+  outercontainer.style.width = width * 600 / 1425 + "px";
+}
+else {
+  element.style.width = width * 90 / 100 + "px";
+  outercontainer.style.width = width * 90 / 100+ "px";
+}
